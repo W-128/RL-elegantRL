1bb1c1665d14776b208bb5e954b178df35b54f16
wyz_glb <wyz@atguigu.com>
2022-04-29 16:57:17 +0800

    step=2800,rtl=2,4,6,8,10,gamma=0.9

--- my_common/utils.py
+++ my_common/utils.py
@@ -159,7 +159,7 @@ def concurrent_request_num_per_second_list_to_concurrent_request_num(
     import uuid
     import csv
     # 先造只有rtl1 和rtl3
-    rtl_list = [3, 5]
+    rtl_list = [2, 4, 6, 8, 10]
 
     request_list = []
     for i in range(len(concurrent_request_num_per_second_list)):
